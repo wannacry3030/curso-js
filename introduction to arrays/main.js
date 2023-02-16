9c1bb04e7821514eddbf2cffd1f9032f00dc0083
wannacry3030 <wannacry.bat@proton.me>
2023-02-16 12:29:57 -0300

    att

--- introduction to arrays/main.js
+++ introduction to arrays/main.js
@@ -7,7 +7,7 @@ const friends = ["ycaro", "peter", "joao"];
 console.log(friends);
 
 //acima fiz uma syntax literal, aqui embaixo chamei a função Array, a forma de cima é mais utilizada..
-const years = new Array(1991, 1983, 2008, 2020);
+const y = new Array(1991, 1983, 2008, 2020);
 
 console.log(friends[0]);
 console.log(friends[1]);
@@ -18,3 +18,26 @@ console.log(friends[friends.length - 1]);
 friends[2] = "jay";
 console.log(friends);
 //aqui nos mudamos uma const, o que acontece: apenas valores primitivos nao podem ser mudados, porem, uma array nao é um valor primitivo
+
+const primeiroNome = "Lucas";
+const lucas = [primeiroNome, "oliveira", 2023 - 1996, "desenvolvedor", friends];
+console.log(lucas);
+console.log(lucas.length);
+
+//exercicio de array (abaixo)
+const calcAge = function (birthYear) {
+  return 2023 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
