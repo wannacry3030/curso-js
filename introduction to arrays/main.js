22f90c5ec92f4021322269eed5283103cc1bba06
wannacry3030 <wannacry.bat@proton.me>
2023-02-16 12:07:00 -0300

    att

--- introduction to arrays/main.js
+++ introduction to arrays/main.js
@@ -5,3 +5,8 @@ const amigo3 = "joao";
 
 const friends = ["ycaro", "peter", "joao"];
 console.log(friends);
+
+//acima fiz uma syntax literal, aqui embaixo chamei a função Array, a forma de cima é mais utilizada..
+const years = new Array(1991, 1983, 2008, 2020);
+
+console.log(friends[0]);
