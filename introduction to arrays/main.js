024f745b707d7b69966cd3e0fced58a4512696a0
wannacry3030 <wannacry.bat@proton.me>
2023-02-17 13:19:08 -0300

    att

--- introduction to arrays/main.js
+++ introduction to arrays/main.js
@@ -100,7 +100,11 @@ const bills = [125, 555, 44];
 
 //3
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(bills, tips);
+
+//4 somando o total em arrays
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
 
 //praticando de novo
 const calcGorjeta = function (conta) {
