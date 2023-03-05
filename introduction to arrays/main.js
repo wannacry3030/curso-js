37029bd577f6a95db3a6f6b25d3897aded6c87cc
wannacry3030 <wannacry.bat@proton.me>
2023-03-05 13:55:42 -0300

    att

--- introduction to arrays/main.js
+++ introduction to arrays/main.js
@@ -459,7 +459,8 @@ const measureKelvin = function () {
     type: "temp",
     unit: "celsius",
     // C : consertando o bug
-    value: Number(prompt("Degrees celsius:")),
+    // value: Number(prompt("Degrees celsius:")),
+    value: 10,
   };
 
   // B : achando o bug
@@ -471,3 +472,23 @@ const measureKelvin = function () {
 };
 // A : identificando o bug
 console.log(measureKelvin());
+
+const calcTempAmplitudebug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudebug = calcTempAmplitudebug([3, 5, 1], [9, 0, 5]);
+console.log(amplitudebug);
